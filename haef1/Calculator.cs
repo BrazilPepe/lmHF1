67d02b537669a829a78ca3428b9f40776294e364
markkoltai <markkoltai@gmail.com>
2017-02-09 11:15:39 +0100

    Hozzáadtam az összeadás függvényt, fogadd sok szeretettel

---
+++ haef1/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace haef1
+{
+    class Calculator
+    {
+        public static double Osszead(double egyikszam, double masikszam)
+        {
+            return egyikszam + masikszam;
+
+        }
+
+
+    }
+}
